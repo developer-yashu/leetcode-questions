f9463d613e044e132dc6d37d978d07479076af5e
yashu sharma <ys4531104@gmail.com>
2025-02-27 20:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.71%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,22 +1,3 @@
-// class Solution {
-// public:
-//     bool isHappy(int n) {
-//         int x=0;
-//         while(n>0){
-//             x+=(n%10)*(n%10);
-//             n/=10;
-//         }
-//         return x;
-//     }
-//     bool ishappy(int n){
-//         while(n>4){
-//             n=sumofdigit(n);
-//         }
-//         return n==1;
-//     }
-// };
-
-
 class Solution {
 public:
     int sumOfSquares(int n) {
