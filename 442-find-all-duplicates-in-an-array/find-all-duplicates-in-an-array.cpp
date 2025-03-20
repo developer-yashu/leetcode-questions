c6e300a87ea6e9043d905f3fcb0d9ff1c8ed5a55
yashu sharma <ys4531104@gmail.com>
2025-03-20 16:21:25 +0530

    Time: 4 ms (67.10%) | Memory: 48.4 MB (76.95%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -4,12 +4,13 @@ public:
         vector<int> result;
         
         for (int i = 0; i < nums.size(); i++) {
-            int index = abs(nums[i]) - 1; // Find index corresponding to value
+            int index = abs(nums[i]) - 1;  
             
             if (nums[index] < 0) {
-                result.push_back(abs(nums[i]));  // Number is already negative, meaning it's a duplicate
-            } else {
-                nums[index] = -nums[index];  // Mark as visited
+                result.push_back(abs(nums[i]));  
+            } 
+            else {
+                nums[index] = -nums[index];  
             }
         }
         
